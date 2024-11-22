ebc0da0ac336a15dfd9594a92b3faef25ef2b00f
Diego-S-G <diego07goncalves@gmail.com>
2024-11-22 13:47:10 -0300

    Index de students.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :students
-  resources :courses
+  resources :students, except: [ :destroy ]
+  # resources :courses
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   
