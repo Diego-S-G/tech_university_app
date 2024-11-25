08c90f7e4320fbd4beca65e8ac63d2224e3c67d3
Diego-S-G <diego07goncalves@gmail.com>
2024-11-25 09:55:49 -0300

    Adiciona senha e confirmação de senha no formulário e controller.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -60,6 +60,6 @@ class StudentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def student_params
-      params.require(:student).permit(:name, :email)
+      params.require(:student).permit(:name, :email, :password, :password_confirmation)
     end
 end
